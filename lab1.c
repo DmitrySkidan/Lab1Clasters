00811b13516af193d3b09fdd90a88cd7a081f9c6
DmitrySkidan <skidan13@gmail.com>
2017-03-01 01:14:50 +0200

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
 
 				if (buf == -1) 
 				{
-					double diff = (MPI_Wtime() - t1) * powf(0.9,n);
+					double diff = 1/(MPI_Wtime() - t1);
 					buf = sum;
 
 					notUsedBuf[countOfNotUsedBuf] = i;
