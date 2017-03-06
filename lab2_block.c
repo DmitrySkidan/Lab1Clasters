5dfd5740773165ba8846fa14c576a6eca62686b9
DmitrySkidan <skidan13@gmail.com>
2017-03-07 01:09:59 +0200

    Update lab2_block.c

--- lab2_block.c
+++ lab2_block.c
@@ -1,7 +1,7 @@
 #include "mpi.h" 
 #include <stdio.h> 
 #include <stdlib.h>
-#define NRA 62 		/* number of rows in matrix A */
+#define NRA 100 		/* number of rows in matrix A */
 #define NCA 15		     /* number of columns in matrix A */ 
 #define NCB 7 		     /* number of columns in matrix B */ 
 #define MASTER 0 	     /* taskid of first task */
